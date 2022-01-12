aa510a6ff6540ccd982519f58a26a6ec451c369f
adefgreen98 <adefgreen98@gmail.com>
2022-01-12 19:06:27 +0100

    Save statistics & modify dropout

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from itertools import product
 
 from utils import *
 from loss import *
-
+from statistics import add_results, build_histogram
 
 """# Main method"""
 
@@ -25,6 +25,7 @@ def main(cfg, **kwargs):
 
     # DATASET
     dataset = get_dataset(kwargs["train_path"])
+    
     train_set, valid_set = split_dataset(dataset, valid_ratio=cfg["valid_ratio"], rnd=False)
     test_dataset = get_dataset(kwargs["test_path"])
     if cfg["valid_ratio"] == 0: valid_set = test_dataset
@@ -33,7 +34,7 @@ def main(cfg, **kwargs):
     valid_dataloader = get_dataloader(valid_set, batch_size=cfg["batch_size"], collate_type='ce')
     
     # MODEL & OTHER OBJECTS
-    embedder = Embedder(kwargs["train_path"])
+    embedder = Embedder(kwargs["train_path"], cfg["embedding_method"])
     net = get_model(embedder, **cfg["model_params"], device=get_device())
     optimizer = get_optimizer(net, cfg["learning_rate"], cfg["optimizer"])
     loss_fn = get_loss(cfg["loss"], embedder)
@@ -43,14 +44,14 @@ def main(cfg, **kwargs):
 
     # SAVING
     _path = 'tests'
-    if kwargs["save"]:
+    if kwargs["_save"]:
         _path = save_model(net, cfg)
         save_training(metrics, _path)
     
     # TESTING
-    test(net, test_dataset, save_path=_path)
-    test_beam_search(net, test_dataset, save_path=_path)
-
+    test_metrics = test(net, test_dataset, save_path=_path)
+    test_beam_metrics = test_beam_search(net, test_dataset, save_path=_path)
+    if kwargs["_save_stats"]: add_results(cfg, test_metrics)
 
 
 def produce_configurations(params):
@@ -67,25 +68,27 @@ parameters = {
     "valid_ratio": [0],
     "batch_size": [64],
     "loss": ["masked_ce"],
-    "optimizer": ["adam", "adamw"],
+    "optimizer": ["adam"],
     "learning_rate": [1e-3],
     "nr_epochs": [15],
     "model_params": {
         "unit_name": ["gru"],
         "hidden_size": [200],
-        "num_layers": [2],
-        "dropout_p": [0.0, 0.5]
-    }
+        "num_layers": [2]
+    },
+    "embedding_method": ['glove']
 }
 
 if __name__ == '__main__':
 
-    for cfg in produce_configurations(parameters):
-        print()
-        print("---> Configuration: <---", *[str(k) + ": " + str(v) for k,v in cfg.items()], sep='\n')
-        main(cfg, train_path = "iob_atis/atis.train.pkl", test_path = "iob_atis/atis.test.pkl", save=True)
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print()
+    for i in range(4):
+        for cfg in produce_configurations(parameters):
+            print()
+            print("---> Configuration: <---", *[str(k) + ": " + str(v) for k,v in cfg.items()], sep='\n')
+            main(cfg, train_path = "iob_atis/atis.train.pkl", test_path = "iob_atis/atis.test.pkl", _save=True, _save_stats=True)
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print()
+    build_histogram('optimizer', 'dropout_p')
 
 
 
