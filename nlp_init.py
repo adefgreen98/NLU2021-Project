ee62bbed6d5d43b275e66cd46ea4f0c14aeb2c82
adefgreen98 <adefgreen98@gmail.com>
2022-01-15 13:54:28 +0100

    Select only relevant vocabulary embeddings

--- nlp_init.py
+++ nlp_init.py
@@ -21,33 +21,34 @@ class TorchEmbedding(object):
     def __init__(self, dataset_path='iob_atis/atis.train.pkl', learn=False, vec_size=300):
         assert vec_size in [50, 100, 200, 300]
         tmpvocab = torchtext.vocab.GloVe(name='6B', dim=vec_size)
-        self.word2idx = tmpvocab.stoi.copy()
-        tmpvectors = tmpvocab.vectors
 
         with open(dataset_path, 'rb') as f:
             _, dataset_dicts = pickle.load(f) 
             vocab = list(dataset_dicts["token_ids"].keys())
 
-        # adding unknown words in dataset
+        # selecting only relevant embeddings
         to_append = []
+        self.word2idx = {}
         for word in vocab:
-            try: _ = self.word2idx[word]
+            self.word2idx[word] = len(to_append)
+            try: 
+                i = tmpvocab.stoi[word]
+                to_append.append(tmpvocab.vectors[i].squeeze())
             except KeyError:
                 to_append.append(torch.randn(vec_size) - 0.5)
-                self.word2idx[word] = len(self.word2idx)
         
         # adding padding words
         self.word2idx['<BOS>'] = len(self.word2idx)
-        to_append.append(torch.zeros(vec_size, dtype=tmpvectors.dtype))
+        to_append.append(torch.zeros(vec_size, dtype=tmpvocab.vectors.dtype))
         
         self.word2idx['<EOS>'] = len(self.word2idx)
-        to_append.append(torch.zeros(vec_size,dtype=tmpvectors.dtype))
+        to_append.append(torch.zeros(vec_size, dtype=tmpvocab.vectors.dtype))
 
         self.word2idx['<PAD>'] = len(self.word2idx)
-        to_append.append(torch.ones(vec_size, dtype=tmpvectors.dtype) * -1)
+        to_append.append(torch.ones(vec_size, dtype=tmpvocab.vectors.dtype) * -1)
 
         # appending new vectors to old ones
-        tmpvectors = torch.cat((tmpvectors, torch.stack(to_append)))
+        tmpvectors = torch.stack(to_append)
 
         self.emb = torch.nn.Embedding.from_pretrained(tmpvectors, freeze=(not learn)).cuda()
         
@@ -77,16 +78,16 @@ class SpacyEmbedding(object):
         v = np.ones_like(nlp.vocab['<BOS>'].vector) * -1
         nlp.vocab.set_vector("<PAD>", v)
 
-        self.nlp = nlp.vocab
+        self.nlp_vocab = nlp.vocab
     
     def __getitem__(self, word):
-        return torch.tensor(self.nlp[word].vector)
-    
-    def get_batch(self, sent):
-        pass
+        return torch.tensor(self.nlp_vocab[word].vector)
 
     def get_size(self):
-        return self.nlp.vectors_length
+        return self.nlp_vocab.vectors_length
+    
+    def get_sent(self, sent):
+        return torch.stack([torch.tensor(self.nlp_vocab[word].vector) for word in sent])
 
 
 def get_preprocessor(method):
