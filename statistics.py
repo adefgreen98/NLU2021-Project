329f433afd6b5cbe74a766c8e8840f8e4a602207
adefgreen98 <adefgreen98@gmail.com>
2022-01-16 18:24:46 +0100

    Correct histogram plot

--- statistics.py
+++ statistics.py
@@ -5,6 +5,7 @@
 import os
 import datetime
 import pandas
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -93,23 +94,80 @@ def dframes_for_attribute(attribute:str):
     return pandas.DataFrame().append(list(values_dframes_dict.values()))
 
 def build_histogram(*args, notfound_option: dict=None, _savefig=False):
+
     for attribute in args:
         assert type(attribute) == str
-        tmp = dframes_for_attribute(attribute)
 
         # Building DataFrame with metric values on each line
-        l = [[{'metric': k, 'value': el[k], attribute: el[notfound_option[attribute]] if el[attribute] == 'not_found' else el[attribute]} for k in el if k != attribute] for el in tmp.to_dict('records')]
-        l = [d for el in l for d in el]
+        l = []
+        for el in dframes_for_attribute(attribute).to_dict('records'):
+            for k in el:
+                if k != attribute:
+                    mdict = {'metric': k, 'value': el[k]}
+                    mdict[attribute] = el[attribute]
+                    if notfound_option is not None: 
+                        if el[attribute] == 'not_found':
+                            mdict[attribute] = notfound_option.get(attribute, 'not_found')
+                    l.append(mdict) 
+
         df = pandas.DataFrame.from_records(l)
         
         # Plotting
-        plt.figure(figsize=(10, 6))
-        ax = sns.barplot(data=df, hue=attribute, x='metric', y='value')
-        for container in ax.containers: ax.bar_label(container, fmt="%.2f", padding=20)
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        plt.suptitle(attribute)
-        plt.legend(loc='upper left', borderaxespad=-1, bbox_to_anchor=(1.02, 1.0))
+        plt.figure(figsize=(10, 10))
+        attr_metrics = set(df['metric'].tolist())
+
+        def order_values(x):
+            def check_float(f:str):
+                if f == 'not_found': return 0
+                try: return float(f)
+                except ValueError: return f
+            
+            def check_int(i:str):
+                if i == 'not_found': return 0
+                try: return int(i)
+                except ValueError: return i
+                
+            attr_values = list(x)
+            test_el = [el for el in attr_values if el != 'not_found'][0]
+            is_f, is_i = check_float(test_el), check_int(test_el)
+            if not (type(is_f) is str):
+                if not (type(is_i) is str): return sorted(attr_values, key=lambda el: check_int(el), reverse=False)
+                else: sorted(attr_values, key=lambda el: check_float(el), reverse=False)
+            else:
+                return attr_values
+            
+        attr_values = order_values(set(df[attribute].tolist()))
+        colors = sns.color_palette('pastel', n_colors=len(attr_values))
+        _step = 4
+        _separation = _step * (len(attr_values) + 1)
+        _reduction = .8
+        _ci = .8
+
+        attr_pos = [list(np.arange(m - (len(attr_values) // 2)*_step , m + (len(attr_values) // 2)*_step + 1, _step)) for m in range(0, _separation*len(attr_metrics), _separation)]
+        for i, m in enumerate(attr_metrics):
+            for j, k in enumerate(attr_values):
+                val_mean = df[(df[attribute] == k) & (df['metric'] == m)]['value'].mean()
+                val_std = df[(df[attribute] == k) & (df['metric'] == m)]['value'].std()
+                container = plt.barh(attr_pos[i][j], val_mean, height=_reduction*_step, label=k, color=colors[j], xerr= _ci * val_std)
+                plt.bar_label(container, fmt="%.2f", padding=int(val_std) + 3)
+
+        plt.legend(attr_values, loc='upper left', bbox_to_anchor=(.9, 1.0))
+        plt.title(attribute)
+        plt.xlabel("Value(%)")
+        plt.ylabel("Metric")
+        plt.xlim((60, 105))
+        plt.yticks(ticks=[pos[len(pos) // 2] for pos in attr_pos], labels=attr_metrics)
+        sns.despine()
+
+        # plt.figure(figsize=(10, 6))
+        # ax = sns.barplot(data=df, hue=attribute, y='metric', x='value', orient='h')
+        # ax.set(xlim=[60, 105])
+        # for container in ax.containers: 
+        #     ax.bar_label(container, fmt="%.2f", padding=25)
+        # ax.spines['right'].set_visible(False)
+        # ax.spines['top'].set_visible(False)
+        # plt.suptitle(attribute)
+        # plt.legend(loc='upper left', borderaxespad=-1, bbox_to_anchor=(1.02, 1.0))
 
 
         if _savefig: plt.savefig(os.path.join(_images_path, datetime.datetime.strftime(datetime.datetime.now(), "%Y_%m_%d_%H_%M_%S") + f"_{attribute}"))
@@ -117,4 +175,5 @@ def build_histogram(*args, notfound_option: dict=None, _savefig=False):
 
 
 if __name__ == '__main__':
-    build_histogram(*os.sys.argv[1:], _savefig=False)
+    # build_histogram(*os.sys.argv[1:], _savefig=False)
+    build_histogram('optimizer', _savefig=False)
