966067c40c57eb2817568c02214129be7d351d86
adefgreen98 <adefgreen98@gmail.com>
2022-01-26 19:22:39 +0100

    Added attention ; rename training main file

---
+++ attention.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+
+from model import Seq2SeqModel, Decoder
+
+class ConcatAttention(nn.Module):
+    """
+    Attention scores computed through a fully-connected layer
+    """
+    def __init__(self, hidden_size, enc_hidden_size, dec_hidden_size):
+        super(ConcatAttention, self).__init__()
+        self.enc_compr = torch.nn.Linear(enc_hidden_size, hidden_size, bias=False)
+        self.dec_compr = torch.nn.Linear(hidden_size, hidden_size, bias=False)
+        self.attention_score = torch.nn.Sequential(*[
+            torch.nn.Linear(hidden_size, 1, bias=False), # feed-forward nn ==> bias needed?
+            torch.nn.Tanh(),
+            torch.nn.Softmax(dim=1)
+        ])
+
+    def forward(self, h_t, h_enc):
+        # h_t --> vector of previous state, of size (1, batch_size, hidden_size)
+        # h_enc --> list of hidden vectors from encoder over the sequence, of size seq_len x (batch_size, hidden_size)
+        seq_len = h_enc.shape[0]
+
+        # putting batch dimension first
+        h_enc = h_enc.permute(1,0,2)
+        h_t = h_t.permute(1,0,2)
+
+        compr_h_enc = self.enc_compr(h_enc)
+        compr_h_t = self.dec_compr(h_t)
+        compr_h_t = torch.cat([compr_h_t for j in range(seq_len)], dim=1) #concatenating along sequence length
+
+        # alphas = []
+        # for i in range(seq_len):
+        #     alphas.append(self.attention_score(compr_h_t[i] + compr_h_enc[i]))
+        # alphas = torch.cat(alphas, dim=-1)
+        alphas = self.attention_score(compr_h_t + compr_h_enc)
+        context = torch.sum(h_enc * alphas, dim=1).unsqueeze(0)
+        return context, alphas
+
+
+class GlobalAttention(nn.Module):
+    """
+    Attention using dot product as scoring function.
+    """
+
+    def __init__(self, decoder_hidden_size):
+        super(GlobalAttention, self).__init__()
+        self.hidden_size = decoder_hidden_size
+
+        # final concatenation layer
+        self.concat = torch.nn.Sequential(*[
+            torch.nn.Linear(2 * self.hidden_size, self.hidden_size, bias=False),
+            torch.nn.Tanh()
+        ])
+    
+    def forward(self, ht, h_enc):
+        # ht --> (1, batch, hidden)
+        # h_enc --> (sequence, batch, hidden)
+        h_enc = h_enc.permute(1,0,2)
+        ht = ht.permute(1,0,2) 
+        alphas = torch.bmm(ht, h_enc.transpose(1,2)).softmax(dim=-1).transpose(1,2)
+        context = torch.sum(h_enc * alphas, dim=1).unsqueeze(0)
+        return context, alphas
+
+
+class LocalAttention(nn.Module):
+    def __init__(self, decoder_hidden_size):
+        super(LocalAttention, self).__init__()
+        self.hidden_size = decoder_hidden_size
+
+        # final concatenation layer
+        self.concat = torch.nn.Sequential(*[
+            torch.nn.Linear(2 * self.hidden_size, self.hidden_size, bias=False),
+            torch.nn.Tanh()
+        ])
+
+
+class AttentionDecoder(Decoder):
+    
+    def __init__(self, input_size, output_size, attention_mode='fcn', **kwargs):
+        super(AttentionDecoder, self).__init__(input_size, output_size, **kwargs)
+        self.attention_mode = attention_mode
+
+        if self.attention_mode == 'concat':
+            # needs to readapt the model for concatenating embedding and context
+            if self.use_embedder: decoder_input_size = self.embedder.weight.shape[1]
+            else: decoder_input_size = self.output_size
+            decoder_input_size = decoder_input_size + self.hidden_size
+            self.model = eval(self.unit_map[self.unit_name])(decoder_input_size, self.hidden_size, num_layers=self.model.num_layers, batch_first=False)
+
+        sw = {
+            'concat': "ConcatAttention(self.hidden_size, self.hidden_size, self.hidden_size)",
+            'local': 'LocalAttention()',
+            'global': 'GlobalAttention(self.hidden_size)'
+        }
+        self.attention = eval(sw[attention_mode])
+        self.global_attn_concat = torch.nn.Sequential(*[
+            torch.nn.Linear(2 * self.hidden_size, self.hidden_size, bias=False),
+            torch.nn.Tanh()
+        ])
+    
+    def forward(self, d_in, prev_h, h_encoder):
+        if self.use_embedder: d_in = self.embedder(d_in)
+        d_in = self.dropout(d_in)
+
+        if self.unit_name == 'lstm': att_h = prev_h[0]
+        else: att_h = prev_h
+        
+        if self.attention_mode == 'concat':
+            context, alphas = self.attention(att_h, h_encoder)
+            x = torch.cat((d_in, context), dim=-1)
+            out, h = self.model(x, prev_h)
+        elif self.attention_mode == 'global':
+            out, h = self.model(d_in, prev_h)
+            if att_h.shape[0] > 1: h_t = att_h[-1].unsqueeze(0)
+            else: h_t = att_h
+            context, alphas = self.attention(h_t, h_encoder) 
+            out = self.global_attn_concat(torch.cat([out, context], dim=-1))
+
+        out = self.dropout(out)
+        res = self.classifier(out.squeeze(0))
+        return res.unsqueeze(0), h
+
+
+
+class AttentionSeq2SeqModel(Seq2SeqModel):
+
+    def __init__(self, attention_mode='fcn', max_seq_len=48, internal_dropout=0.0, **kwargs):
+        super(AttentionSeq2SeqModel, self).__init__(**kwargs)
+        self.attention_mode = attention_mode
+
+        if self.decoder_input_mode == 'sentence':
+            self.decoder = AttentionDecoder(self.input_size, self.output_size, hidden_size=self.decoder_hidden_size, 
+                dropout=internal_dropout, num_layers=self.num_layers, 
+                unit=self.unit_name, attention_mode=self.attention_mode
+                ).to(self.device)
+
+        elif self.decoder_input_mode == 'label_embed':
+            # add one embedding vector for start of sentence 
+            self.decoder = AttentionDecoder(self.output_size + 1, self.output_size, hidden_size=self.decoder_hidden_size, 
+            dropout=internal_dropout, num_layers=self.num_layers, unit=self.unit_name, 
+            use_embedder=True, attention_mode=self.attention_mode
+            ).to(self.device)
+
+        else:
+            self.decoder = AttentionDecoder(self.output_size, self.output_size, hidden_size=self.decoder_hidden_size, 
+            dropout=internal_dropout, num_layers=self.num_layers, 
+            unit=self.unit_name, attention_mode=self.attention_mode
+            ).to(self.device)
+
---
+++ training_main.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+from itertools import product
+
+from utils import *
+from loss import *
+from statistics import add_results, build_histogram
+
+"""# Main method"""
+
+def main(cfg, **kwargs):
+
+    # DATASET
+    dataset = get_dataset(kwargs["train_path"])
+    
+    train_set, valid_set = split_dataset(dataset, valid_ratio=0.4, rnd=True)
+    test_dataset = get_dataset(kwargs["test_path"])
+    
+    train_dataloader = get_dataloader(train_set, batch_size=cfg["batch_size"], shuffle=True)
+    valid_dataloader = get_dataloader(valid_set, batch_size=cfg["batch_size"])
+    test_dataloader = get_dataloader(test_dataset, batch_size=cfg["batch_size"])
+    
+    # MODEL & OTHER OBJECTS
+    net = get_model(**cfg["model_params"], device=get_device())
+    optimizer = get_optimizer(net, cfg["learning_rate"], cfg["optimizer"])
+    loss_fn = get_loss('')
+    
+    # TRAINING
+    net, metrics = train(cfg["nr_epochs"], net, train_dataloader, optimizer, loss_fn, valid_dl=valid_dataloader, greedy_save=True)
+
+    # SAVING
+    _path = 'tests'
+    if kwargs["_save"]:
+        _path = save_model(net, cfg)
+        save_training(metrics, _path)
+    
+    try: net = load_model(os.path.join(_path, 'model.pth'), from_config=cfg["model_params"])
+    except FileNotFoundError:
+        try: net = load_model('checkpoint.pth', from_config=cfg["model_params"])
+        except FileNotFoundError: pass
+
+    # GREEDY TESTING
+    test_metrics = test(net, test_dataloader)
+
+    # # BEAM-SEARCH TESTING
+    # beam_widths = [6, 8]
+    # test_beam_metrics = {}
+    # for w in beam_widths:
+    #     test_beam_metrics[w] = test_beam_search(net, test_dataset, save_path=_path, beam_width=w)
+    
+    if kwargs["_save_stats"]: 
+        add_results(cfg, test_metrics)
+        # for w in beam_widths:
+        #     add_results(dict(**cfg, beam_width=w), test_beam_metrics[w])
+
+
+def produce_configurations(params):
+    tmp = params.copy()
+    param_names = list(tmp.keys())
+    if 'model_params' in tmp: tmp["model_params"] = list(produce_configurations(tmp["model_params"]))
+    configurations = product(*list(tmp.values()))
+
+    for cfg in configurations:
+        yield {k: v for k, v in zip(param_names, cfg)}
+
+
+parameters = {
+    "batch_size": [64],
+    "optimizer": ["adamw"],
+    "learning_rate": [1e-3],
+    "nr_epochs": [30],
+    "model_params": {
+        "bidirectional": [True],
+        "embedding_method": ['glove'],
+        "unit_name": ["lstm"],
+        "hidden_size": [256],
+        "num_layers": [2],
+        "decoder_input_mode": ['label'],
+        "intermediate_dropout": [0.0],
+        "internal_dropout": [0.0]
+    },
+   
+}
+
+attn_parameters = {
+    "batch_size": [64],
+    "optimizer": ["adamw"],
+    "learning_rate": [1e-3],
+    "nr_epochs": [30],
+    "model_params": {
+        'num_layers': [2],
+        "bidirectional": [True],
+        "embedding_method": ['spacy', 'glove'],
+        "unit_name": ["lstm"],
+        "hidden_size": [256],
+        "decoder_input_mode": ['sentence'],
+        "attention_mode": ['global'],
+    }
+
+}
+
+# parameters = attn_parameters
+
+if __name__ == '__main__':
+    iterations = 1
+    for i in range(iterations):
+        _cfgs = list(produce_configurations(parameters))
+        for j, cfg in enumerate(_cfgs):
+            print(f"---> Iteration {i + 1}/{iterations}, Configuration {j + 1}/{len(_cfgs)} <---") 
+            # print(*[str(k) + ": " + str(v) for k,v in cfg.items()], sep='\n')
+            pprint(cfg)
+            main(cfg, train_path = "iob_atis/atis.train.pkl", test_path = "iob_atis/atis.test.pkl", _save=False, _save_stats=False)
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print()
+    
+    # build_histogram('optimizer', 'intermediate_dropout', 'internal_dropout', 'beam_width', 'learning_rate', 'unit_name',
+    #         notfound_option=dict(beam_width='1', internal_dropout='0.0', intermediate_dropout='0.0'), 
+    #         _savefig=True
+    #         )
+
+
+
